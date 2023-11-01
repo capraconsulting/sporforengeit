a9275e1256a42a66fa1934c24c7fc18f835ba94b
Kharann <hey@akvo.no>
2023-11-01 15:01:36 +0100

    add typing to stuff

--- src/randomAnimal.ts
+++ src/randomAnimal.ts
@@ -3,12 +3,12 @@ import { ANIMALS } from "./animals";
 
 const numAnimalNames = ANIMALS ? ANIMALS.length : 0;
 
-export const randomAnimal = (seed) => {
+export const randomAnimal = (seed?: string) => {
   if (seed === undefined) {
-    const getRandomInt = (min, max) => {
-      min = Math.ceil(min);
-      max = Math.floor(max);
-      return Math.floor(Math.random() * (max - min + 1)) + min;
+    const getRandomInt = (min: number, max: number) => {
+      const min2 = Math.ceil(min);
+      const max2 = Math.floor(max);
+      return Math.floor(Math.random() * (max2 - min2 + 1)) + min2;
     };
     const animal = ANIMALS[getRandomInt(0, numAnimalNames - 1)];
     return `Anonym ${animal}`;
