3b57cf350fb09306b38c8faf29168bd690df72ad
Kharann <hey@akvo.no>
2023-11-06 19:02:18 +0100

    minify code

--- infra/slackbot-stack.ts
+++ infra/slackbot-stack.ts
@@ -2,7 +2,7 @@ import { Construct } from "constructs";
 
 import { CfnOutput, Stack, StackProps } from "aws-cdk-lib";
 import { AttributeType, Table } from "aws-cdk-lib/aws-dynamodb";
-import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
+import { NodejsFunction, SourceMapMode } from "aws-cdk-lib/aws-lambda-nodejs";
 import { FunctionUrlAuthType, Runtime } from "aws-cdk-lib/aws-lambda";
 import * as path from "path";
 
@@ -23,6 +23,12 @@ export class SlackbotStack extends Stack {
 				SLACK_SIGNING_SECRET: process.env.SLACK_SIGNING_SECRET || "",
 				SLACK_BOT_TOKEN: process.env.SLACK_BOT_TOKEN || "",
 			},
+			bundling: {
+				minify: true,
+				sourceMap: true,
+				sourcesContent: false,
+				target: "es2018",
+			}
 		});
 		dynamoTable.grantReadWriteData(lambda);
 
