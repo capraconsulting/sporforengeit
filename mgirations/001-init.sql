4f0a5066546ef6e4fae9a7e9191539ecfed799a7
Kharann <hey@akvo.no>
2023-12-04 12:41:14 +0100

    remove sql and storing data

--- mgirations/001-init.sql
+++
@@ -1,8 +0,0 @@
-create table messages (
-    id integer primary key,
-    content text,
-    authorId text,
-    scheduled boolean default false, 
-    published boolean default false,
-    createdAt text
-);
